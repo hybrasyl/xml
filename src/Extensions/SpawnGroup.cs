771919be8940d0627bde6d60d125444205ebae3c
Justin Baugh <justin.baugh@eris.co>
2023-04-16 12:33:58 -0700

    move files to src; use base class in xsd2code settings

---
+++ src/Extensions/SpawnGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Hybrasyl.Xml.src;
+
+namespace Hybrasyl.Xml.Objects;
+
+public partial class SpawnGroup : HybrasylLoadable, IHybrasylLoadable<SpawnGroup>
+{
+    public static string Directory => "spawngroups";
+
+    public static XmlLoadResponse<SpawnGroup> LoadAll(string baseDir)
+    {
+        var ret = new XmlLoadResponse<SpawnGroup>();
+        foreach (var xml in GetXmlFiles(Path.Join(baseDir, Directory)))
+            try
+            {
+                var group = LoadFromFile(xml);
+                ret.Results.Add(group);
+            }
+            catch (Exception e)
+            {
+                ret.Errors.Add(xml, e.ToString());
+            }
+
+        return ret;
+    }
+
+    public static SpawnGroup operator +(SpawnGroup sg1, SpawnGroup sg2)
+    {
+        var merged = sg1.Clone<SpawnGroup>();
+        merged.Spawns.AddRange(sg2.Spawns);
+        return merged;
+    }
+}
