7d7042d475b917e370454206ebc4106ac314a98e
Justin Baugh <justin.baugh@eris.co>
2023-05-01 09:45:43 -0700

    durr

--- src/Objects/MonsterFormulaSet.cs
+++ src/Objects/MonsterFormulaSet.cs
@@ -6,56 +6,56 @@
 #pragma warning disable
 namespace Hybrasyl.Xml.Objects
 {
-using System;
-using System.Diagnostics;
-using System.Xml.Serialization;
-using System.Runtime.Serialization;
-using System.Collections;
-using System.Xml.Schema;
-using System.ComponentModel;
-using System.Xml;
-using System.IO;
-using System.Text;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
+    using System;
+    using System.Diagnostics;
+    using System.Xml.Serialization;
+    using System.Runtime.Serialization;
+    using System.Collections;
+    using System.Xml.Schema;
+    using System.ComponentModel;
+    using System.Xml;
+    using System.IO;
+    using System.Text;
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
 
-[System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.8.4161.0")]
-[Serializable]
-[DebuggerStepThrough]
-[DesignerCategoryAttribute("code")]
-[XmlTypeAttribute(Namespace="http://www.hybrasyl.com/XML/Hybrasyl/2020-02")]
-public partial class MonsterFormulaSet : HybrasylEntity<MonsterFormulaSet>
-{
-    #region Private fields
-    private List<Formula> _formula;
-    private string _name;
-    #endregion
-    
-    [XmlElement("Formula")]
-    public List<Formula> Formula
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.8.4161.0")]
+    [Serializable]
+    [DebuggerStepThrough]
+    [DesignerCategoryAttribute("code")]
+    [XmlTypeAttribute(Namespace = "http://www.hybrasyl.com/XML/Hybrasyl/2020-02")]
+    public partial class MonsterFormulaSet : HybrasylEntity<MonsterFormulaSet>
     {
-        get
+        #region Private fields
+        private List<Formula> _formula;
+        private string _name;
+        #endregion
+
+        [XmlElement("Formula")]
+        public List<Formula> Formula
         {
-            return _formula;
+            get
+            {
+                return _formula;
+            }
+            set
+            {
+                _formula = value;
+            }
         }
-        set
+
+        [XmlAttribute(DataType = "token")]
+        public string Name
         {
-            _formula = value;
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
         }
     }
-    
-    [XmlAttribute(DataType="token")]
-    public string Name
-    {
-        get
-        {
-            return _name;
-        }
-        set
-        {
-            _name = value;
-        }
-    }
-}
 }
 #pragma warning restore
